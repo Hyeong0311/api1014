09fef6a94a27e6029747125a05263b8e89a9c058
jhyeong <hyeongcode@gmail.com>
2024-10-15 13:23:15 +0900

    1323

--- src/main/java/org/hyeong/api1014/security/config/CustomSecurityConfig.java
+++ src/main/java/org/hyeong/api1014/security/config/CustomSecurityConfig.java
@@ -2,12 +2,15 @@ package org.hyeong.api1014.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class CustomSecurityConfig {
 
     @Bean
@@ -18,6 +21,11 @@ public class CustomSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.formLogin(config -> config.disable());
+
+        http.sessionManagement(config ->
+                config.sessionCreationPolicy(SessionCreationPolicy.NEVER));
 
         http.authorizeRequests(
                 (auth -> auth.requestMatchers("/api/v1/product/list").permitAll())
