c22fc82b8d9bb6036666bc910e7484d646a93ee7
jhyeong <hyeongcode@gmail.com>
2024-10-15 14:20:35 +0900

    1420

--- src/main/java/org/hyeong/api1014/member/service/MemberService.java
+++ src/main/java/org/hyeong/api1014/member/service/MemberService.java
@@ -2,9 +2,15 @@ package org.hyeong.api1014.member.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.hyeong.api1014.common.exception.CommonExceptions;
+import org.hyeong.api1014.member.domain.MemberEntity;
+import org.hyeong.api1014.member.dto.MemberDTO;
 import org.hyeong.api1014.member.repository.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,5 +20,27 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    private final PasswordEncoder passwordEncoder;
 
+    public MemberDTO authenticate(String email, String password) {
+
+        Optional<MemberEntity> result = memberRepository.findById(email);
+
+        MemberEntity member = result.orElseThrow(() -> CommonExceptions.READ_ERROR.get());
+
+        String enPw = member.getPw();
+
+        boolean match = passwordEncoder.matches(password, enPw);
+
+        if(!match) {
+            throw CommonExceptions.READ_ERROR.get();
+        }
+
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setEmail(email);
+        memberDTO.setPw(enPw);
+        memberDTO.setRole(member.getRole().toString());
+
+        return memberDTO;
+    }
 }
