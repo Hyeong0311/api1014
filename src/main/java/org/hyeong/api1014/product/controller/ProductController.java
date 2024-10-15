093d7920fd58904e1ad14fd9eb95897c64d39bb0
jhyeong <hyeongcode@gmail.com>
2024-10-15 10:49:50 +0900

    1049

---
+++ src/main/java/org/hyeong/api1014/product/controller/ProductController.java
@@ -0,0 +1,25 @@
+package org.hyeong.api1014.product.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.hyeong.api1014.common.dto.PageRequestDTO;
+import org.hyeong.api1014.common.dto.PageResponseDTO;
+import org.hyeong.api1014.product.dto.ProductListDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/product")
+@Log4j2
+public class ProductController {
+
+    @GetMapping("list")
+    public ResponseEntity<PageResponseDTO<ProductListDTO>> list(
+            @Validated PageRequestDTO pageRequestDTO
+    ) {
+
+        return ResponseEntity.ok(null);
+    }
+}
