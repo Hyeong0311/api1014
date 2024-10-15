bda9c9762de1e3769332798ed470ec87af9ea8d7
jhyeong <hyeongcode@gmail.com>
2024-10-15 11:47:45 +0900

    1147

--- src/main/java/org/hyeong/api1014/common/advice/CommonControllerAdvice.java
+++ src/main/java/org/hyeong/api1014/common/advice/CommonControllerAdvice.java
@@ -1,11 +1,13 @@
 package org.hyeong.api1014.common.advice;
 
+
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.hyeong.api1014.common.exception.TaskException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +17,7 @@ import java.util.Map;
 public class CommonControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,Object>> handle(MethodArgumentNotValidException ex){
+    public ResponseEntity<java.util.Map<String,Object>> handle(MethodArgumentNotValidException ex){
 
         BindingResult bindingResult = ex.getBindingResult();
 
@@ -33,4 +35,13 @@ public class CommonControllerAdvice {
         return ResponseEntity.status(400).body(map);
 
     }
+
+    @ExceptionHandler(TaskException.class)
+    public ResponseEntity<String> handle(TaskException ex){
+
+        return ResponseEntity.status(ex.getCode()).body(ex.getMessage());
+
+    }
+
+
 }
---
+++ src/main/java/org/hyeong/api1014/product/service/ProductService.java
@@ -0,0 +1,27 @@
+package org.hyeong.api1014.product.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.hyeong.api1014.common.dto.PageRequestDTO;
+import org.hyeong.api1014.common.dto.PageResponseDTO;
+import org.hyeong.api1014.common.exception.CommonExceptions;
+import org.hyeong.api1014.product.dto.ProductListDTO;
+
+@Service
+@Transactional
+@Log4j2
+@RequiredArgsConstructor
+public class ProductService {
+
+    public PageResponseDTO<ProductListDTO> list(PageRequestDTO pageRequestDTO) {
+
+        if(pageRequestDTO.getPage() < 0) {
+            throw CommonExceptions.LIST_ERROR.get();
+        }
+
+        return null;
+    }
+
+}
