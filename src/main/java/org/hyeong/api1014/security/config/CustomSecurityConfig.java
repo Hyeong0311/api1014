551588fa33739f3264598989eb66998dc23fcec7
jhyeong <hyeongcode@gmail.com>
2024-10-15 10:43:02 +0900

    1043

--- src/main/java/org/hyeong/api1014/security/config/CustomSecurityConfig.java
+++ src/main/java/org/hyeong/api1014/security/config/CustomSecurityConfig.java
@@ -2,8 +2,10 @@ package org.hyeong.api1014.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -12,6 +14,18 @@ public class CustomSecurityConfig {
     public PasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(
+                (auth -> auth.requestMatchers("/api/v1/category").permitAll())
+        );
+
+        http.csrf(config -> config.disable());
+
+        return http.build();
     }
 }
 
