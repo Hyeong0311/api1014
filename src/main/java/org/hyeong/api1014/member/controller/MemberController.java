64d1e96aee3617c0aabb2c7ba62a00c0728e0264
jhyeong <hyeongcode@gmail.com>
2024-10-15 15:16:59 +0900

    1516

--- src/main/java/org/hyeong/api1014/member/controller/MemberController.java
+++ src/main/java/org/hyeong/api1014/member/controller/MemberController.java
@@ -1,8 +1,11 @@
 package org.hyeong.api1014.member.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.hyeong.api1014.member.dto.MemberDTO;
 import org.hyeong.api1014.member.dto.TokenRequestDTO;
 import org.hyeong.api1014.member.dto.TokenResponseDTO;
+import org.hyeong.api1014.member.service.MemberService;
+import org.hyeong.api1014.security.util.JWTUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +13,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/member")
 @Log4j2
 public class MemberController {
+
+    private final MemberService memberService;
+
+    private final JWTUtil jwtUtil;
+
+    public MemberController(MemberService memberService, JWTUtil jwtUtil) {
+        this.memberService = memberService;
+        this.jwtUtil = jwtUtil;
+    }
 
     @PostMapping("makeToken")
     public ResponseEntity<TokenResponseDTO> makeToken(
@@ -22,6 +36,22 @@ public class MemberController {
         log.info("===============================");
         log.info("Make Token");
 
-        return null;
+        MemberDTO memberDTO =
+                memberService.authenticate(tokenRequestDTO.getEmail(), tokenRequestDTO.getPw());
+
+        log.info(memberDTO);
+
+        Map<String, Object> claimMap =
+                Map.of("email", memberDTO.getEmail(), "role", memberDTO.getRole());
+
+        String accessToken = jwtUtil.createToken(claimMap, 5);
+        String refreshToken = jwtUtil.createToken(claimMap, 360);
+
+        TokenResponseDTO tokenResponseDTO = new TokenResponseDTO();
+        tokenResponseDTO.setAccessToken(accessToken);
+        tokenResponseDTO.setRefreshToken(refreshToken);
+        tokenResponseDTO.setEmail(memberDTO.getEmail());
+
+        return ResponseEntity.ok(tokenResponseDTO);
     }
 }
