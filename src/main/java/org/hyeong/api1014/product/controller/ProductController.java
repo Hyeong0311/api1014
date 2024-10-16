ef065f25691786e7b190c059cab3247f0bef4826
jhyeong <hyeongcode@gmail.com>
2024-10-16 10:36:07 +0900

    1036

--- src/main/java/org/hyeong/api1014/product/controller/ProductController.java
+++ src/main/java/org/hyeong/api1014/product/controller/ProductController.java
@@ -1,9 +1,11 @@
 package org.hyeong.api1014.product.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.hyeong.api1014.common.dto.PageRequestDTO;
 import org.hyeong.api1014.common.dto.PageResponseDTO;
 import org.hyeong.api1014.product.dto.ProductListDTO;
+import org.hyeong.api1014.product.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
@@ -14,15 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/product")
 @Log4j2
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PreAuthorize(("permitAll()"))
 //    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("list")
     public ResponseEntity<PageResponseDTO<ProductListDTO>> list(
-            @Validated PageRequestDTO pageRequestDTO
+            @Validated PageRequestDTO requestDTO
     ) {
 
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(productService.list(requestDTO));
     }
 }
--- src/main/java/org/hyeong/api1014/product/service/ProductService.java
+++ src/main/java/org/hyeong/api1014/product/service/ProductService.java
@@ -2,6 +2,7 @@ package org.hyeong.api1014.product.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.hyeong.api1014.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.hyeong.api1014.common.dto.PageRequestDTO;
@@ -15,13 +16,15 @@ import org.hyeong.api1014.product.dto.ProductListDTO;
 @RequiredArgsConstructor
 public class ProductService {
 
+    private final ProductRepository productRepository;
+
     public PageResponseDTO<ProductListDTO> list(PageRequestDTO pageRequestDTO) {
 
         if(pageRequestDTO.getPage() < 0) {
             throw CommonExceptions.LIST_ERROR.get();
         }
 
-        return null;
+        return productRepository.listByCno(0L, pageRequestDTO);
     }
 
 }
