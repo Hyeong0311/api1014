bb4302776f98f46184a950bae533cbeb81a7b5ad
jhyeong <hyeongcode@gmail.com>
2024-10-16 11:38:01 +0900

    1137

--- src/main/java/org/hyeong/api1014/security/config/CustomSecurityConfig.java
+++ src/main/java/org/hyeong/api1014/security/config/CustomSecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.hyeong.api1014.security.filter.JWTCheckFilter;
 import org.hyeong.api1014.security.util.JWTUtil;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 @Configuration
@@ -36,8 +41,28 @@ public class CustomSecurityConfig {
 
         http.csrf(config -> config.disable());
 
+        http.cors(cors -> {
+            cors.configurationSource(corsConfigurationSource());
+        });
+
         http.addFilterBefore(new JWTCheckFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOriginPatterns(List.of("*")); // 어디서든 허락
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
